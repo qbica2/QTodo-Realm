59408db438ed68c613f205750ac7677f17182289
Kubilay Akdemir <kubidik27@gmail.com>
2023-07-15 15:03:05 +0300

    create AddNewCategoryButton in HomeScreen

--- QTodo-Realm/Screens/HomeScreen.swift
+++ QTodo-Realm/Screens/HomeScreen.swift
@@ -6,17 +6,48 @@
 //
 
 import SwiftUI
+import RealmSwift
 
 struct HomeScreen: View {
+    
+    @ObservedResults(CategoryModel.self) var categoryList
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            if categoryList.isEmpty {
+                Text("Empty List")
+            } else {
+                
+            }
         }
-        .padding()
+        .toolbar {
+            ToolbarItem(placement: .bottomBar) {
+                AddNewCategoryButton
+            }
+        }
     }
+}
+
+//MARK: - Subviews
+
+extension HomeScreen {
+    
+    var AddNewCategoryButton: some View {
+        
+        Button {
+            
+        } label: {
+            Text("Add Category")
+                .tint(.white)
+                .padding()
+                .background(Color.pink.opacity(0.8).ignoresSafeArea(edges: .bottom))
+                .cornerRadius(12)
+                .shadow(radius: 12)
+                .padding()
+        }
+
+    }
+
 }
 
 struct HomeScreen_Previews: PreviewProvider {
