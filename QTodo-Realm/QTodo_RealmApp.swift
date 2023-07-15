28c8247b40f1b9dca6b7d8a72726fb824acbd204
Kubilay Akdemir <kubidik27@gmail.com>
2023-07-15 14:03:57 +0300

    Initial Commit

---
+++ QTodo-Realm/QTodo_RealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  QTodo_RealmApp.swift
+//  QTodo-Realm
+//
+//  Created by Mehmet Kubilay Akdemir on 15.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct QTodo_RealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
